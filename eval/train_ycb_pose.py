9928f76f8cb07b680b72cd09f64e7caee905c6ba
padeler <padeler@ics.forth.gr>
2020-03-27 00:32:01 +0200

    take2 ycb. clean-up.

--- eval/train_ycb_pose.py
+++ eval/train_ycb_pose.py
@@ -5,22 +5,25 @@ def run():
     sets = [
             # "NbClass_06__TrainFrames_03_Frames",
             # "NbClass_06__TrainFrames_01_Percent",
-            "NbClass_06__TrainFrames_05_Percent",
-            "NbClass_06__TrainFrames_10_Percent",
-            "NbClass_06__TrainFrames_20_Percent",
+            # "NbClass_06__TrainFrames_05_Percent",
+            # "NbClass_06__TrainFrames_10_Percent",
+            # "NbClass_06__TrainFrames_20_Percent",
             # "NbClass_06__TrainFrames_50_Percent",
+
             # "NbClass_20__TrainFrames_03_Frames",
             # "NbClass_20__TrainFrames_01_Percent",
             "NbClass_20__TrainFrames_05_Percent",
             "NbClass_20__TrainFrames_10_Percent",
             "NbClass_20__TrainFrames_20_Percent",
             # "NbClass_20__TrainFrames_50_Percent",
+
             # "NbClass_50__TrainFrames_03_Frames",
             # "NbClass_50__TrainFrames_01_Percent",
             "NbClass_50__TrainFrames_05_Percent",
             "NbClass_50__TrainFrames_10_Percent",
             "NbClass_50__TrainFrames_20_Percent",
             # "NbClass_50__TrainFrames_50_Percent",
+
             # "NbClass_92__TrainFrames_03_Frames",
             # "NbClass_92__TrainFrames_01_Percent",
             "NbClass_92__TrainFrames_05_Percent",
